ccf4b80a42ba1656d4c1e95dfca349441de31a90
Bogdan Marinescu <bogdan.marinescu@arm.com>
2015-07-30 15:12:32 +0100

    Use regular sleep instead of deep sleep
    In the absence of a mechanism that would allow us to choose the best sleep
    mode at runtime, we're forced to use the least restrictive one, otherwise
    internal peripherals that might be in use will not work at all.

--- source/sleep.c
+++ source/sleep.c
@@ -21,14 +21,7 @@
 
 void mbed_enter_sleep(sleep_t *obj)
 {
-    /* NOTE: this should me mcg_clock_select_t mcg_clock = CLOCK_HAL_GetClkSrcMode(MCG_BASE);
-       However, at least with arm-none-eabi-gcc 4.8.3 20131129, the above call ends up doing a 32 bit access to the MCG
-       registers, which results in a hard fault, since the MCG registers only accept 8 bit accesses apparently.
-       Newer version of KSDK changed the way they access the MCG, so that should be fixed, but until we update to
-       a newer KSDK, the code below should provide a compiler-agnostic fix
-    */
-    obj->mcg_clock = (mcg_clock_select_t)(((*(volatile uint8_t*) HW_MCG_C1_ADDR(MCG_BASE)) & BM_MCG_C1_CLKS) >> BP_MCG_C1_CLKS);
-
+    (void)obj;
     smc_power_mode_protection_config_t sleep_config = {
         .vlpProt = true,            /*!< VLP protect*/
         .llsProt = true,            /*!< LLS protect */
@@ -38,21 +31,14 @@ void mbed_enter_sleep(sleep_t *obj)
 #endif
     };
     SMC_HAL_SetProtection(SMC_BASE, &sleep_config);
-    SMC->PMCTRL = SMC_PMCTRL_STOPM(2);
 
-    //Deep sleep for ARM core:
-    SCB->SCR = 1 << SCB_SCR_SLEEPDEEP_Pos;
+    //Regular sleep for ARM core:
+    SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk;
     __WFI();
 }
 
 void mbed_exit_sleep(sleep_t *obj)
 {
-    //Switch back to PLL as clock source if needed
-    //The interrupt that woke up the device will run at reduced speed
-    if (obj->mcg_clock == kMcgClkSelOut) {
-        if (CLOCK_HAL_GetPllStatMode(MCG_BASE) == kMcgPllStatPllClkSel) {
-            while (CLOCK_HAL_GetLock0Mode(MCG_BASE) == kMcgLockUnlocked);
-        }
-        CLOCK_HAL_SetClkSrcMode(MCG_BASE, kMcgClkSelOut);
-    }
+    (void)obj;
 }
+
